6643eef382ca8b9b08e7296593466dcf35814ed2
Zacharie Chenail-Larcher <corlapidem@gmail.com>
2022-06-06 21:31:34 -0400

    commande d'encouragement v1 de base

---
+++ src/functions/encourage/main.py
@@ -0,0 +1,21 @@
+import random
+import functions_framework
+
+encouragements = [
+    "Courage ",
+    "Tu peux y arriver ",
+    "T'es le/la/lo meilleur(e) ",
+    "T'es le/la/lo plus vraiment meilleur(e) du monde ",
+    "Les froges sont avec toi ",
+    "Tu mets du soleil dans nos coeurs "]
+
+
+@functions_framework.http
+def encourage(request):
+    """HTTP Cloud Function."""
+    request_json = request.get_json(silent=True)
+    if request_json:
+        name = request_json.get("name", None)
+        if name:
+            return random.choice(encouragements) + f"<@{name}> <3!"
+    return "Hello !"
