5efafee7503d74d7d94466df17aef6842aab127f
Armand <armand.123@live.fr>
2022-06-08 15:11:17 -0400

    ! Remove useless timeout

--- src/functions/froge/index.js
+++ src/functions/froge/index.js
@@ -8,7 +8,11 @@ const { Client, Intents } = require('discord.js');
 
 exports.froge = (req, res) => {
     // Create a new client instance
-    const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS] });
+    const client = new Client({
+        intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS]
+    });
+    // Login to Discord with your client's token
+    client.login(process.env.DISCORD_TOKEN);
 
     // When the client is ready, run this code (only once)
     client.once("ready", () => {
@@ -18,18 +22,17 @@ exports.froge = (req, res) => {
             // Get all emojis starting with "froge"
             froges = client.emojis.cache.filter(emoji => emoji.name.match("froge*"));
             froges = Array.from(froges);
-            
+
             // Select a random one
             selectedFroge = froges[Math.floor(Math.random() * froges.length)];
 
-            // Return that one
-            message = await channel.send(`${selectedFroge[1]}`);
+            // Return the lucky one
+            await channel.send(`${selectedFroge[1]}`);
             console.log("Done!");
-            setTimeout((() => { client.destroy(); res.end() }), 100)
+
+            // Kill the client
+            client.destroy();
+            return;
         });
     });
-
-    // Login to Discord with your client's token
-    client.login(process.env.DISCORD_TOKEN);
-
 };
