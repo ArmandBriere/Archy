604d691cdfc6945972a2f53385e133a7461fd4c2
Armand <armand.123@live.fr>
2022-06-08 23:17:41 -0400

    ! Constant on top

--- src/functions/gif/main.py
+++ src/functions/gif/main.py
@@ -1,13 +1,12 @@
 import functions_framework
 
+DEFAULT_GIF = "https://tenor.com/view/the-rock-hmmm-gif-21574594"
+UNKNOWN_GIF = "https://tenor.com/view/bof-i-dont-mind-i-dont-understand-why-jean-dujardin-oss117-gif-20383956"
+
 gifs = {
     "doubt": "https://tenor.com/view/doubt-press-x-la-noire-meme-x-button-gif-19259237",
     "confused": "https://tenor.com/view/confusion-chicken-gif-11299790",
 }
-
-UNKNOWN_GIF = "https://tenor.com/view/bof-i-dont-mind-i-dont-understand-why-jean-dujardin-oss117-gif-20383956"
-
-DEFAULT_GIF = "https://tenor.com/view/the-rock-hmmm-gif-21574594"
 
 
 @functions_framework.http
