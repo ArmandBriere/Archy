0715aeb7df46eab9bda1544ab53e54ad5b03582d
Armand Briere <armand.123@live.fr>
2022-06-10 15:30:52 -0400

    ! Linter 2 space

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,22 @@
 
 variable "project_id" {
-    default = "archy-f06ed"
+  default = "archy-f06ed"
+}
+
+variable "project_name" {
+  default = "archy"
 }
 
 variable "region" {
-    default = "us-central1"
+  default = "us-central1"
 }
 
 variable "service_account_email" {
-    default = "archyapi@archy-f06ed.iam.gserviceaccount.com"
+  default = "archyapi@archy-f06ed.iam.gserviceaccount.com"
 }
 
 variable "secrets" {
-  type = list
+  type = list(any)
   default = [
     "DISCORD_TOKEN",
     "GOOGLE_APPLICATION_CREDENTIALS"
@@ -20,66 +24,66 @@ variable "secrets" {
 }
 
 variable "python_functions" {
-  type = map
+  type = map(any)
   default = {
-    describe: {
+    describe : {
       description = "Describe a user"
-      runtime = "python39"
-      timeout = 15
-      memory = 256
+      runtime     = "python39"
+      timeout     = 15
+      memory      = 256
     },
-    exp: {
+    exp : {
       description = "Increase the experience of a user"
-      runtime = "python39"
-      timeout = 15
-      memory = 256
+      runtime     = "python39"
+      timeout     = 15
+      memory      = 256
     },
-    hello: {
+    hello : {
       description = "Simple hello"
-      runtime = "python39"
-      timeout = 15
-      memory = 256
+      runtime     = "python39"
+      timeout     = 15
+      memory      = 256
     },
-    js: {
+    js : {
       description = "Template of a function in javascript"
-      runtime = "nodejs16"
-      timeout = 15
-      memory = 256
+      runtime     = "nodejs16"
+      timeout     = 15
+      memory      = 256
     },
-    level: {
+    level : {
       description = "Return the level of a user"
-      runtime = "python39"
-      timeout = 15
-      memory = 256
+      runtime     = "python39"
+      timeout     = 15
+      memory      = 256
     }
-    froge: {
+    froge : {
       description = "Return a random froge from the server"
-      runtime = "nodejs16"
-      timeout = 15
-      memory = 512
+      runtime     = "nodejs16"
+      timeout     = 15
+      memory      = 512
     }
-    gif: {
+    gif : {
       description = "Return the requested gif"
-      runtime = "python39"
-      timeout = 15
-      memory = 256
+      runtime     = "python39"
+      timeout     = 15
+      memory      = 256
     }
   }
 }
 
 # Provider to connect to Google
 provider "google" {
-    project = var.project_id
-    region  = var.region
+  project = var.project_id
+  region  = var.region
 }
 
 # Bucket to store the function code
 resource "google_storage_bucket" "function_bucket" {
-    name     = "${var.project_id}-function"
-    location = var.region
+  name     = "${var.project_id}-function"
+  location = var.region
 }
 
 resource "google_storage_bucket" "input_bucket" {
-    name     = "${var.project_id}-input"
-    location = var.region
+  name     = "${var.project_id}-input"
+  location = var.region
 }
