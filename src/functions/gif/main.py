fe195770f76c417db62b193d6395650a0e1e06fb
Zacharie C-L <55606963+Cor-Lapidem@users.noreply.github.com>
2022-06-27 16:07:39 -0400

    Gif command Tenor api (#4)
    * + Main node in gcloud vm with command in serverless functions
    
    * + Freeze missing requirements, forward id instead of name
    
    * + Exp function and lot of fixes
    
    * + All commands are processed now, they all post to cloud based functions
    
    * + New level function
    
    * + Setup tox with black, pylint, isort and pytest
    
    * + Doc on how to run tox
    
    * + Tests run with vscode now
    
    * + New basic document on how to write and run test with mock
    
    * + New exp test with mock
    
    * ! Remove unused param, replace by MagicMock()
    
    * + Fix typo and change tests
    
    * + New decris_moi command
    
    * + Package needed for arch based os
    
    * + New JS function to have a discord instance in a function
    
    * + Rename describe and error handling for level
    
    * + First terraform for cloud function
    
    * + change to list with env var
    
    * + ENV var as for each, all cloud function added, fix JS code with env var
    
    * + Setup terraform backend in gcs
    
    * + Change to sha and ignore pycache folder
    
    * + New level system with exp tracking, more test of exp with mock
    
    * + New froge js command
    
    * + Pass command params to functions
    
    * + VsCode function debug config, fix main node
    
    * commande de gif og no db + test
    
    * commande d'encouragement v1 de base
    
    * ! Fix bad url and setup terraform
    
    * ! Remove useless timeout
    
    * correction
    
    * tox
    
    * + New Who dares me code on @bot tag
    
    * ! Constant on top
    
    * ! Change default gif
    
    * + Code coverage of test is now active, increase coverage to 100%
    
    * + User ranks based on total exp
    
    * + Rank and embed mesage for function return
    
    * tenor api gif fct
    
    * tox tenor api
    
    * eng
    
    * ajustements
    
    * adjust and full test cover, success tox
    
    * resolve conflict part 1
    
    * part 2
    
    * delete old readme
    
    * delete file
    
    * delete file
    
    * delete file
    
    * delete file
    
    * delete encourage
    
    * put back good readme src
    
    * delete file
    
    * delete file
    
    * adjust main gif file
    
    * adjust test gif
    
    * tuple return values
    
    * replace hard coded test
    
    * + Mock http call, add more typing and some small refactor
    
    Co-authored-by: Armand Briere <armand.123@live.fr>

--- src/functions/gif/main.py
+++ src/functions/gif/main.py
@@ -1,10 +1,15 @@
-from typing import Any, Optional
+import json
+import os
+from typing import Any, Optional, Tuple
 
+import flask
 import functions_framework
+import requests
 
 DEFAULT_GIF = "https://tenor.com/view/frog-multiply-gif-25342428"
 UNKNOWN_GIF = "https://tenor.com/view/bof-i-dont-mind-i-dont-understand-why-jean-dujardin-oss117-gif-20383956"
 
+# This will be added to firebase in the future
 gifs = {
     "doubt": "https://tenor.com/view/doubt-press-x-la-noire-meme-x-button-gif-19259237",
     "confused": "https://tenor.com/view/confusion-chicken-gif-11299790",
@@ -12,15 +17,38 @@ gifs = {
 
 
 @functions_framework.http
-def gif(request):
-    """HTTP Cloud Function."""
+def gif(request: flask.Request) -> Tuple[str, int]:
+    """Return the requested gif or the first found with the tenor API."""
 
     request_json: Optional[Any] = request.get_json(silent=True)
     if request_json:
-        params = request_json.get("params", [""])
-        if len(params) == 0:
-            return DEFAULT_GIF
+        params: list = request_json.get("params", [""])
+
+        if len(params) == 0 or params[0] == "":
+            return DEFAULT_GIF, 200
+
         if params[0].lower() in gifs:
-            return gifs[params[0]]
-        return UNKNOWN_GIF
-    return DEFAULT_GIF
+            return gifs[params[0]], 200
+
+        api_key: str = os.environ["TENOR_API_TOKEN"]
+        response: requests.Response = requests.get(
+            f"https://tenor.googleapis.com/v2/search?q={params[0].lower()}&key={api_key}&client_key=Archy&limit=1"
+        )
+
+        return extract_data_from_response(response.status_code, response.content), 200
+
+    return DEFAULT_GIF, 200
+
+
+def extract_data_from_response(response_status: int, response_content: bytes) -> str:
+    """Function that deals with the api request result"""
+
+    if response_status == 200:
+        top_gifs = json.loads(response_content)
+
+        try:
+            return top_gifs["results"][0]["media_formats"]["gif"]["url"]
+        except KeyError:
+            return "https://tenor.com/view/404-not-found-error-20th-century-fox-gif-24907780"
+
+    return UNKNOWN_GIF
