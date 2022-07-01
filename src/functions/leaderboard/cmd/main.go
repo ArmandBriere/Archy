018d979965fa962b24e86b886344586c05bc507b
Pierre-Marc <pmdameus@gmail.com>
2022-07-01 16:31:33 -0400

    Add: add cmd/main.go to allow testing on function leaderboard

---
+++ src/functions/leaderboard/cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+
+	leaderboard "leaderboard.com/cloudfunction"
+)
+
+func main(){
+	//Add custom server_id to test
+	body := []byte(`{"server_id" : "<SERVER_ID>"}`)
+
+	reader := bytes.NewReader(body)
+	req := httptest.NewRequest(http.MethodGet, "/test", reader)
+	w := httptest.NewRecorder()
+
+	// Call function
+	leaderboard.sendLeaderboardUrl(w,req)
+}
