739f28cde76c4e9d2c852af676c243f988b8def7
Pierre-Marc <pmdameus@gmail.com>
2022-07-01 16:24:40 -0400

    fix: remove useless import and send the correct url

--- src/functions/leaderboard/main.py
+++ src/functions/leaderboard/main.py
@@ -2,8 +2,8 @@ from typing import Any, Optional, Tuple
 
 import flask
 import functions_framework
-from google.cloud.firestore_v1.client import Client
-from google.cloud.firestore_v1.document import DocumentReference, DocumentSnapshot
+
+BASE_URL = "https://archybot.web.app/leaderboard/"
 
 
 @functions_framework.http
@@ -18,10 +18,6 @@ def leaderboard(request: flask.Request) -> Tuple[str, int]:  # pylint: disable=W
             print("Exit: Missing data in payload")
             return "", 200
 
-        database: Client = Client(project="archy-f06ed")
-        doc_ref: DocumentReference = database.collection("servers").document(server_id)
-        doc: DocumentSnapshot = doc_ref.get()
-
-        return str(doc.get("url"))
+        return f"{BASE_URL}/{server_id}"
 
     return "", 200
