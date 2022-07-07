17bc60dbb8126384d8e6981c125679315e4bdb81
Armand <armand.123@live.fr>
2022-07-07 08:33:36 -0400

    + add unit test and fix code

--- src/functions/describe/main.py
+++ src/functions/describe/main.py
@@ -13,10 +13,14 @@ def describe(request):
         user_id = request_json.get("user_id", None)
         mentions = request_json.get("mentions", None)
 
+        if user_id is None or user_id == "":
+            return "I don't even know who you are!", 200
+
         # Special user are protected. This will go with firebase in the future
         if mentions:
-            if mentions[0] == 135048445097410560:
-                return "This person is awesome!"
+            if str(mentions[0]) == "135048445097410560":
+                return "This person is awesome!", 200
+
             return f"<@{mentions[0]}> is a big ****!", 200
 
         return f"You are a big ****! <@{user_id}>", 200
