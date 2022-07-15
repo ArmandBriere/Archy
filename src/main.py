d5371fe3322a05e821e772d83bbc31e49b3991c3
Pierre-Marc Dameus <pmdameus@gmail.com>
2022-07-15 13:18:45 -0400

    Feature/add new user join greeting event (#40)

--- src/main.py
+++ src/main.py
@@ -2,12 +2,15 @@ import json
 import logging
 import os
 import re
+from datetime import datetime
 
 import firebase_admin
 import google.oauth2.id_token
 import requests
 from discord import Embed
+from discord.abc import GuildChannel
 from discord.ext.commands import Bot, Context
+from discord.member import Member as member_type
 from discord.message import Message as message_type
 from dotenv import load_dotenv
 from firebase_admin import credentials, firestore
@@ -24,6 +27,7 @@ load_dotenv()
 LOGGER: logging.Logger = logging.getLogger(__name__)
 DISCORD_API_TOKEN = os.getenv("DISCORD_API_TOKEN")
 FUNCTION_BASE_RUL = "https://us-central1-archy-f06ed.cloudfunctions.net/"
+DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 # Discord bot settings
 bot: Bot = Bot(command_prefix="!", description="Serverless commands discord bot")
@@ -50,6 +54,76 @@ def is_active_command(server_id: str, command_name: str) -> bool:
     return False
 
 
+def create_user(member: member_type) -> bool:
+    """Create a user in the firestore db."""
+    server_id = str(member.guild.id)
+    user_collection: CollectionReference = db.collection("servers").document(server_id).collection("users")
+    doc_ref: DocumentReference = user_collection.document(str(member.id))
+    doc: DocumentSnapshot = doc_ref.get()
+
+    # If user doesn't exist, create it with user_id as the document id
+    if not doc.exists:
+        doc_ref.set(
+            {
+                "total_exp": 0,
+                "exp_toward_next_level": 0,
+                "level": 0,
+                "message_count": 0,
+                "last_message_timestamp": datetime.now().strftime(DATETIME_FORMAT),
+                "username": str(member.name),
+                "avatar_url": str(member.avatar_url),
+            }
+        )
+
+        return True
+
+    # If user exists, return False
+    return False
+
+
+@bot.event
+async def on_member_join(member: member_type) -> None:
+    LOGGER.warning("Member %s has just joined the server %s", member.name, member.guild.name)
+
+    project_id = "archy-f06ed"
+    topic_id = "channel_message_discord"
+
+    # Get targeted channel
+    server_id = str(member.guild.id)
+    channel_collection: CollectionReference = db.collection("servers").document(server_id).collection("channels")
+    doc_ref: DocumentReference = channel_collection.document("welcome")
+    doc: DocumentSnapshot = doc_ref.get()
+
+    if doc.exists:
+        channel: GuildChannel = member.guild.get_channel(int(doc.get("channel_id")))
+
+        if channel is None:
+            LOGGER.warning(
+                "Channel id %s doesn't exist anymore or is invalid for document welcome for server %s",
+                str(doc.get("channel_id")),
+                str(member.guild.name),
+            )
+        else:
+            # Publish the message to the topic
+            publisher = PublisherClient()
+            topic_path = publisher.topic_path(project_id, topic_id)
+            data = {
+                "channel_id": str(channel.id),
+                "message": f"Welcome {member.display_name} to the server {member.guild.name}!",
+            }
+
+            user_encode_data: bytes = json.dumps(data, indent=2).encode("utf-8")
+
+            future: Future = publisher.publish(topic_path, user_encode_data)
+
+    # Create user in firestore db if doesn't exist
+    is_new_user = create_user(member)
+
+    print(f"Message id: {future.result()}")
+    print(f"Published message to {topic_path}.")
+    print(f"User created: {is_new_user}")
+
+
 @bot.event
 async def on_message(message: message_type) -> None:
     LOGGER.warning("Message from %s is: %s", message.author, message.content)
@@ -62,6 +136,7 @@ async def on_message(message: message_type) -> None:
 
         function_path = f"{FUNCTION_BASE_RUL}{ctx.invoked_with}"
         google_auth_token = google.oauth2.id_token.fetch_id_token(request, function_path)
+
         response: Response = requests.post(
             function_path,
             headers={
