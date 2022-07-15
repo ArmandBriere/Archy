688e96773e2acdbde96c519d1a4266d83a41a2b1
Zacharie C-L <55606963+Cor-Lapidem@users.noreply.github.com>
2022-07-15 17:04:26 -0400

    add insult to terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -114,6 +114,14 @@ variable "http_functions" {
       memory      = 256
       secrets     = []
     },
+    insult : {
+      description = "Return an insult using the LibInsult API (https://insult.mattbas.org/api/)"
+      runtime     = "python39"
+      entry_point = "insult"
+      timeout     = 15
+      memory      = 256
+      secrets     = []
+    },
   }
 }
 
