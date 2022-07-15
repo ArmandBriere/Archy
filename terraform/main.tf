09fef4b84328672c094c9151110f523a784eaa79
Zacharie C-L <55606963+Cor-Lapidem@users.noreply.github.com>
2022-07-15 17:16:45 -0400

    no discord token needed

--- terraform/main.tf
+++ terraform/main.tf
@@ -120,7 +120,7 @@ variable "http_functions" {
       entry_point = "insult"
       timeout     = 15
       memory      = 256
-      secrets     = ["DISCORD_TOKEN"]
+      secrets     = []
     },
   }
 }
