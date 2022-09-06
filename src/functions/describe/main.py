3e70c7dcc2fefc85d33381784bb8f046392908da
cj291059 <cj291059@ens.uqam.ca>
2022-09-06 17:21:48 -0400

    Added new awesome person to describe cmd

--- src/functions/describe/main.py
+++ src/functions/describe/main.py
@@ -18,7 +18,7 @@ def describe(request):
 
         # Special user are protected. This will go with firebase in the future
         if mentions:
-            if str(mentions[0]) == "135048445097410560" or str(mentions[0]) == "964693548396068916":
+            if str(mentions[0]) == "135048445097410560" or str(mentions[0]) == "964693548396068916" or str(mentions[0]) == "127947460462116864":
                 return "This person is awesome!", 200
 
             return f"<@{mentions[0]}> is a big ****!", 200
