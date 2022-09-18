34144851e82a576d56a113e08c2b46bcae9be2b4
Armand Briere <46636172+ArmandBriere@users.noreply.github.com>
2022-09-18 13:13:19 -0400

    Feature/improve help (#72)
    * + Improve help command design and start script to populate Firestore
    
    * + Finish register command script
    
    * + Finish register command script
    
    * + Remove const and use a proper db query
    
    * + Formating change

--- src/functions/help/main.py
+++ src/functions/help/main.py
@@ -5,6 +5,7 @@ import functions_framework
 from google.cloud.firestore_v1.client import Client
 from google.cloud.firestore_v1.collection import CollectionReference
 from google.cloud.firestore_v1.document import DocumentSnapshot
+from google.cloud.firestore_v1.query import Query
 
 
 @functions_framework.http
@@ -14,8 +15,9 @@ def help(request: flask.Request) -> Tuple[str, int]:  # pylint: disable=W0622
     request_json: Optional[Any] = request.get_json(silent=True)
     if request_json:
         server_id = request_json.get("server_id", None)
+        server_name = request_json.get("server_name", None)
 
-        if not server_id:
+        if not server_id or not server_name:
             print("Exit: Missing data")
             return "", 200
 
@@ -23,8 +25,18 @@ def help(request: flask.Request) -> Tuple[str, int]:  # pylint: disable=W0622
         function_collection: CollectionReference = (
             database.collection("servers").document(server_id).collection("functions")
         )
-        docs: Generator[DocumentSnapshot, Any, None] = function_collection.where("active", "==", True).stream()
+        docs: Generator[DocumentSnapshot, Any, None] = (
+            function_collection.where("active", "==", True).order_by("name", direction=Query.ASCENDING).stream()
+        )
 
-        return "\n".join([f"!{doc.id} -> {doc.get('description')}" for doc in docs])
+        response = f"Avaible commands for Archy in **{server_name}**:\n"
+        for doc in docs:
+            response += f"\n**!{doc.id}**\n"
+            response += f"{doc.get('description')}\n"
+            for example in doc.get("examples"):
+                response += f"`{example}`\n"
+            response += "\n"
+
+        return response
 
     return "", 200
