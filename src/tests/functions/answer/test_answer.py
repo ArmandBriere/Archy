9a668a944ec24d17719e83e8ca5dbb8af762232e
Lachance, Alexandre <alexandrelachance@me.com>
2022-09-21 18:00:56 -0400

    Remove useless Mock

--- src/tests/functions/answer/test_answer.py
+++ src/tests/functions/answer/test_answer.py
@@ -1,12 +1,8 @@
-from unittest.mock import MagicMock
-
 from functions.answer.main import POSSIBLE_ANSWERS, answer
 
 
 def test_hello():
-    request_mock = MagicMock()
-
-    result = answer(request_mock)
+    result = answer({})
 
     assert result[0] in POSSIBLE_ANSWERS
     assert result[1] == 200
