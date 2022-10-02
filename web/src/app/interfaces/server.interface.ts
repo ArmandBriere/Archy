212f666e498d4e79500ef78aeb0073a043f3e914
Armand Briere <armand.123@live.fr>
2022-10-01 20:51:49 -0400

    + add total user and hide join button on missing data

--- web/src/app/interfaces/server.interface.ts
+++ web/src/app/interfaces/server.interface.ts
@@ -1,6 +1,7 @@
 export interface ServerInterface {
     server_name: string;
     server_id: string;
-    server_banner: string;
-    join: string;
+    server_icon: string;
+    member_count: number;
+    join?: string;
 }
