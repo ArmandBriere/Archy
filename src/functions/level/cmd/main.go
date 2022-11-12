bab89f04732f840a1a16c28bb614c7f6a8ae6061
Armand Briere <46636172+ArmandBriere@users.noreply.github.com>
2022-11-11 22:58:37 -0500

    Feature/slash level (#115)
    * + Levelfaster is now Level
    
    * + register level slash command
    
    * + Update terraform
    
    * + Remove levelfaster
    
    * + Delete dead code and lower pytest fail-under

--- src/functions/level/cmd/main.go
+++ src/functions/level/cmd/main.go
@@ -5,15 +5,14 @@ import (
 	"net/http"
 	"net/http/httptest"
 
-	levelFaster "levelFaster.com/cloudfunction"
+	levelFaster "level.com/cloudfunction"
 )
 
 func main() {
 	body := []byte(`{
-		"server_id": "", 
-		"user_id": "",
-		"mentions": [""]}
-	`)
+		"server_id": "<SERVER_ID>", 
+		"user_id": "<USER_ID>"
+	}`)
 	reader := bytes.NewReader(body)
 
 	req := httptest.NewRequest(http.MethodGet, "/test", reader)
