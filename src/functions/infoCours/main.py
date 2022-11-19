5ea465a5213908463162a23c877f3e1c6607b503
Student42069 <lehackdeshackers@gmail.com>
2022-11-19 11:52:21 -0500

    Add getChannelName function

--- src/functions/infoCours/main.py
+++ src/functions/infoCours/main.py
@@ -1,15 +1,24 @@
 import flask
 import functions_framework
 
+def getChannelName(request):
+    request_json = request.get_json(silent=True)
+    channelName = request_json.get("server_name", None)
+    if channelName.include("#"):
+        channelName.replace("#", "")
+    return lower(channelName)
+
 @functions_framework.http
-def infoCours(request: flask.Request) -> Tuple[str, int]:
+def infoCours(request):
+    requs
     """This is a template function that show how to send back message."""
+    
 
     request_json: Optional[Any] = request.get_json(silent=True)
 
     if request_json:
         user_id = request_json.get("user_id", None)
         if user_id:
-            return f"Hello <@{user_id}>!", 200
+            return f"Hello Monsieur <@{user_id}>!", 200
 
     return "Hello Monsieur!", 200
