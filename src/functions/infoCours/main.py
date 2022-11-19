f4c7e11e64133f2be253d799ccac3e18d5e12789
Student42069 <lehackdeshackers@gmail.com>
2022-11-19 11:30:14 -0500

    test

---
+++ src/functions/infoCours/main.py
@@ -0,0 +1,15 @@
+import flask
+import functions_framework
+
+@functions_framework.http
+def infoCours(request: flask.Request) -> Tuple[str, int]:
+    """This is a template function that show how to send back message."""
+
+    request_json: Optional[Any] = request.get_json(silent=True)
+
+    if request_json:
+        user_id = request_json.get("user_id", None)
+        if user_id:
+            return f"Hello <@{user_id}>!", 200
+
+    return "Hello Monsieur!", 200
