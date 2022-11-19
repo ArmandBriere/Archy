dff7fea2f8486449241e53e7535695cd42159cd7
Pascal Laprade <laprade.p@gmail.com>
2022-11-19 11:39:10 -0500

    Added more settings to tsconfig

--- src/functions/ts/index.ts
+++ src/functions/ts/index.ts
@@ -3,9 +3,9 @@ import { Request, Response } from 'express'
 /**
  * Responds to any HTTP request.
  *
- * @param req HTTP request context.
+ * @param _ HTTP request context.
  * @param res HTTP response context.
  */
-exports.js = (req: Request, res: Response) => {
+exports.js = (_: Request, res: Response) => {
   res.status(200).send("TypeScript has a good type system!")
 };
