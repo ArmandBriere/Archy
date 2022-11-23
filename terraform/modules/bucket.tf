ee050cfa24d4545f8f336094bf8d1f5d61e2830f
Armand Briere <46636172+ArmandBriere@users.noreply.github.com>
2022-11-23 16:00:40 -0500

    + Make bucket private (#153)

--- terraform/modules/bucket.tf
+++ terraform/modules/bucket.tf
@@ -1,13 +1,8 @@
-resource "google_storage_bucket_access_control" "public_rule" {
-  bucket = google_storage_bucket.froge.name
-  role   = "READER"
-  entity = "allUsers"
-}
-
 resource "google_storage_bucket" "froge" {
-  name          = "froge-public-bucket-${var.environment}"
-  location      = var.region
-  storage_class = "STANDARD"
+  name                     = "froge-bucket-${var.environment}"
+  location                 = var.region
+  storage_class            = "STANDARD"
+  public_access_prevention = "enforced"
 }
 
 resource "google_storage_bucket" "function_bucket" {
