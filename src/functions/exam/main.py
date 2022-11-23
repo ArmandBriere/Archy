94326b9ba5b0b37398203b44e31d812398871cea
Armand Briere <46636172+ArmandBriere@users.noreply.github.com>
2022-11-22 19:09:06 -0500

    Hackathon team and multi environment (#129)
    * Feature/prod dev env (#118)
    
    * Feature/env in code (#121)
    
    * video command  (#120)
    
    * + Add a call to the update user roles cloud function on level up (#125)
    
    * + Create cloud sink that redirect to cloud_function_error_log (#127)
    
    * Feature/hackathon team (#130)
    
    * strict safe search (#131)
    
    * go slash command (#116)
    
    * 53 modifiy main node slash commands (#108)
    
    * + delete teams (#145)
    
    * improve level function (#146)
    
    * improved level command by treating image in cloud and returning base 64 as requested in #122
    
    * fixed level slash command (#147)
    
    * exam command to display exam dates (#133)
    
    * modified slash commands to go from the original 3 second time limit to 15 minutes (#151)
    
    Co-authored-by: Armand Briere <armand.123@live.fr>
    Co-authored-by: Zacharie C-L <55606963+Cor-Lapidem@users.noreply.github.com>
    Co-authored-by: Elouan Hildgen-Poirier <elouan112@gmail.com>
    Co-authored-by: Student42069 <lehackdeshackers@gmail.com>
    Co-authored-by: wassimkha <43592500+wassimkha@users.noreply.github.com>
    Co-authored-by: Raouf Baaziz <45836036+raoufbaz@users.noreply.github.com>

---
+++ src/functions/exam/main.py
@@ -0,0 +1,91 @@
+from datetime import date, datetime
+from typing import Any, Optional
+
+import flask
+import functions_framework
+import requests
+from bs4 import BeautifulSoup
+
+
+def examens_scraper(course: str, semester: str) -> list[str]:
+    """Function that scrapes schedule website for exam dates"""
+
+    current_year = str(date.today().year)
+    url_link = f"https://etudier.uqam.ca/wshoraire/cours/{course}/{current_year}{semester}/7316"
+    try:
+        result = requests.get(url_link).text
+        soup = BeautifulSoup(result, "html.parser")
+        name_box = soup.find(lambda tag: tag.name == "li" and "Examens" in tag.text)
+        name = name_box.text.strip()
+    except AttributeError:
+        return []
+    if name is None:
+        return []
+    return name.replace("Examens: ", "").split("et")
+
+
+semester_id = {
+    "not_found": "0",
+    "winter": "1",
+    "summer": "2",
+    "fall": "3",
+}
+
+
+def get_semester() -> str:
+    """Function that returns the current semester"""
+
+    current_year = str(date.today().year)
+    fall_start, fall_end = f"01-09-{current_year}", f"12-21-{current_year}"
+    winter_start, winter_end = f"01-01-{current_year}", f"04-30-{current_year}"
+    summer_start, summer_end = f"01-05-{current_year}", f"08-30-{current_year}"
+    date_today = date.today()
+    current_date = datetime(date_today.year, date_today.month, date_today.day)
+
+    if datetime.strptime(winter_start, "%m-%d-%Y") <= current_date <= datetime.strptime(winter_end, "%m-%d-%Y"):
+        return semester_id["winter"]
+    if datetime.strptime(summer_start, "%m-%d-%Y") <= current_date <= datetime.strptime(summer_end, "%m-%d-%Y"):
+        return semester_id["summer"]
+    if datetime.strptime(fall_start, "%m-%d-%Y") <= current_date <= datetime.strptime(fall_end, "%m-%d-%Y"):
+        return semester_id["fall"]
+    return semester_id["not_found"]
+
+
+def get_channel_name(request_json: Optional[Any]) -> str:
+    """Function that returns the current channel name"""
+
+    channel_name: str = request_json.get("channel_name", None)
+    if not channel_name:
+        return ""
+    return channel_name.lower()
+
+
+def display_exams(course: str, semester: str) -> str:
+    """Function that returns message to be displayed to the channel"""
+
+    exams = examens_scraper(course, semester)
+    message = ""
+
+    if not exams:
+        return "Sorry, I didn't find anything"
+    if len(exams) == 1:
+        message += f"Final: {exams[0]}\n"
+    else:
+        message += "Vos exams: \n"
+        message += f"Intra: {exams[0]}\n"
+        message += f"Final: {exams[1]}\n"
+    return message
+
+
+@functions_framework.http
+def exam(request: flask.Request) -> str:
+    """Function that displays exam info from current semester for a channel"""
+
+    request_json: Optional[Any] = request.get_json(silent=True)
+
+    if request_json:
+        semester = get_semester()
+        course = get_channel_name(request_json)
+        message = display_exams(course, semester)
+        return message
+    return "Sorry, I didn't find anything"
