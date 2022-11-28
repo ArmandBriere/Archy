10a79b9c272ed34581689690e0b42781e0472e72
Armand Briere <46636172+ArmandBriere@users.noreply.github.com>
2022-11-28 09:56:01 -0500

    Revert "+ Make bucket private (#153)" (#155)
    This reverts commit ee050cfa24d4545f8f336094bf8d1f5d61e2830f.

--- terraform/modules/bucket.tf
+++ terraform/modules/bucket.tf
@@ -1,8 +1,13 @@
+resource "google_storage_bucket_access_control" "public_rule" {
+  bucket = google_storage_bucket.froge.name
+  role   = "READER"
+  entity = "allUsers"
+}
+
 resource "google_storage_bucket" "froge" {
-  name                     = "froge-bucket-${var.environment}"
-  location                 = var.region
-  storage_class            = "STANDARD"
-  public_access_prevention = "enforced"
+  name          = "froge-public-bucket-${var.environment}"
+  location      = var.region
+  storage_class = "STANDARD"
 }
 
 resource "google_storage_bucket" "function_bucket" {
