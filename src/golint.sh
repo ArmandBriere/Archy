7f812c0cb2ea1ca3e000e04989c9af527d67fc62
Armand <armand.123@live.fr>
2023-02-23 12:47:58 -0500

    + Golint local and CI runs on the same config now

--- src/golint.sh
+++ src/golint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-find . -name "*.go" -not -path "*/cmd/*.go" -execdir pwd \; -execdir $(go env GOPATH)/bin/golangci-lint run --go=1.18 \;
+find . -name "*.go" -not -path "*/cmd/*.go" -execdir pwd \; -execdir /usr/bin/golangci-lint run --go=1.18 --config=../../.golangci.yml \;
