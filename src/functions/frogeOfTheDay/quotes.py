b1d1b7c214d233aaf61c5d4fb4fb6782b61d8b35
Armand Briere <armand.123@live.fr>
2023-08-27 13:02:26 -0400

    feat: split quotes into files

---
+++ src/functions/frogeOfTheDay/quotes.py
@@ -0,0 +1,254 @@
+AUTHORS = {
+    "zactrixo": "zactrixo#8903",
+    "jaycee": "Jay Cee#5430",
+    "firelex": "FireLexFtw#8683",
+    "hannibal": "Hannibal119#3744",
+    "moonscrub": "moonscrub#5366",
+    "coder4life": "coder4life#4678",
+    "arthe": "arth-e#0399",
+    "grisamah": "Grisamah#2143",
+    "yannick": "Yannick#5937",
+    "opdelta": "opdelta#1665",
+    "biereAuGibier": "BièreAuGibier Brigand#2022",
+    "eDavid": "EDav1123#7479",
+    "osmiumpeach": "osmiumpeach#1127",
+}
+
+QUOTES = [
+    {
+        "quote": "All my homies hate javascript",
+        "author": AUTHORS["zactrixo"],
+    },
+    {
+        "quote": "Throw sh*t at the wall until it sticks",
+        "author": AUTHORS["zactrixo"],
+    },
+    {
+        "quote": "BlueJ is actually NOT underrated! It's really not good...",
+        "author": AUTHORS["jaycee"],
+    },
+    {
+        "quote": "Rust is love, Rust is life.",
+        "author": AUTHORS["firelex"],
+    },
+    {
+        "quote": "C# is like java, but its better",
+        "author": AUTHORS["zactrixo"],
+    },
+    {
+        "quote": "Fais ton TP",
+        "author": AUTHORS["hannibal"],
+    },
+    {
+        "quote": "Code it",
+        "author": AUTHORS["hannibal"],
+    },
+    {
+        "quote": "Git gud",
+        "author": AUTHORS["hannibal"],
+    },
+    {
+        "quote": "damn archy is down again",
+        "author": AUTHORS["firelex"],
+    },
+    {
+        "quote": "rakabicyk katredmi ?",
+        "author": AUTHORS["moonscrub"],
+    },
+    {
+        "quote": "entRE Quote PleaSe sinON Je PreNDs pas LeS meSsagES",
+        "author": AUTHORS["firelex"],
+    },
+    {
+        "quote": "My love for you is stronger than an infinite loop",
+        "author": AUTHORS["coder4life"],
+    },
+    {
+        "quote": "je suis juste ici pour les bidoux",
+        "author": AUTHORS["arthe"],
+    },
+    {
+        "quote": "..... how did everything broke",
+        "author": AUTHORS["grisamah"],
+    },
+    {
+        "quote": "j'comprends pas les pointeurs",
+        "author": AUTHORS["grisamah"],
+    },
+    {
+        "quote": "why are we using javasript again?",
+        "author": AUTHORS["grisamah"],
+    },
+    {
+        "quote": "À l'ÉTS ya assez de prises pour tout le monde dans les salles de cours !",
+        "author": AUTHORS["moonscrub"],
+    },
+    {
+        "quote": "Si vous avez besoin d'aide en INF3105, regardez les capsules d'Hannibal",
+        "author": AUTHORS["moonscrub"],
+    },
+    {
+        "quote": "Froge Jesus will save us all! Amen!",
+        "author": AUTHORS["moonscrub"],
+    },
+    {
+        "quote": "Alex a en fait un grand cœur !",
+        "author": AUTHORS["moonscrub"],
+    },
+    {
+        "quote": "You all need Froge Jesus!",
+        "author": AUTHORS["moonscrub"],
+    },
+    {
+        "quote": "Tokébakicitte",
+        "author": AUTHORS["moonscrub"],
+    },
+    {
+        "quote": "Faites vos noeuds en 8 comme il faut avant de faire la voie !",
+        "author": AUTHORS["moonscrub"],
+    },
+    {
+        "quote": "Oubliez pas de regarder si ya un tour de friction avant d'assurer !",
+        "author": AUTHORS["moonscrub"],
+    },
+    {
+        "quote": "Si j'peux faire une injection sql sur ton app, tu vas avoir un beau 0 !",
+        "author": AUTHORS["moonscrub"],
+    },
+    {
+        "quote": "Haskell is actually underrated!",
+        "author": AUTHORS["moonscrub"],
+    },
+    {
+        "quote": "Come contribute to my code and add some new features!",
+        "author": AUTHORS["moonscrub"],
+    },
+    {
+        "quote": "Come check my source code on github",
+        "author": AUTHORS["moonscrub"],
+    },
+    {
+        "quote": "Il semble que les services informatiques de l'UQÀM ont besoin de financement supplémentaire",
+        "author": AUTHORS["moonscrub"],
+    },
+    {
+        "quote": "Http/1.1 Service Unavailable - Portail UQÀM Janvier 2023",
+        "author": AUTHORS["yannick"],
+    },
+    {
+        "quote": "Shrodinger's portail étudiant",
+        "author": AUTHORS["opdelta"],
+    },
+    {
+        "quote": "TikTok beurk, Logiciel espion pour le gouv chinois !",
+        "author": AUTHORS["moonscrub"],
+    },
+    {
+        "quote": "Je vous conseille de ne pas utiliser TikTok.",
+        "author": AUTHORS["moonscrub"],
+    },
+    {
+        "quote": "With C it doesn't work and you don't know why. With python it works but you don't know why!"
+        + " So C is better.",
+        "author": AUTHORS["moonscrub"],
+    },
+    {
+        "quote": "Programming is like sex: One mistake and you have to support it for the rest of your life.",
+        "author": AUTHORS["arthe"],
+    },
+    {
+        "quote": "Fun fact: Archy is the greatest discord bot in the world",
+        "author": AUTHORS["moonscrub"],
+    },
+    {
+        "quote": "Ouais ben tsé, j'expérimentais la semaine passée sur un microprocesseur à cycle unique x86...",
+        "author": AUTHORS["moonscrub"],
+    },
+    {
+        "quote": "Le serveur du bot serverless est down",
+        "author": AUTHORS["yannick"],
+    },
+    {
+        "quote": "?.... it works",
+        "author": AUTHORS["grisamah"],
+    },
+    {
+        "quote": "There are two hard things in computer science: cache invalidation, naming things,"
+        + " and off-by-one errors.",
+        "author": AUTHORS["opdelta"],
+    },
+    {
+        "quote": "Http/1.1 Service Unavailable",
+        "author": AUTHORS["yannick"],
+    },
+    {
+        "quote": "déppartement",
+        "author": AUTHORS["grisamah"],
+    },
+    {
+        "quote": "Plannet",
+        "author": AUTHORS["jaycee"],
+    },
+    {
+        "quote": "Bon Fuck it, we need more quotes...",
+        "author": AUTHORS["hannibal"],
+    },
+    {
+        "quote": "Exp",
+        "author": AUTHORS["eDavid"],
+    },
+    {
+        "quote": "And that's why we need a NSFW channel",
+        "author": AUTHORS["opdelta"],
+    },
+    {
+        "quote": "JayCee got beaten by a 5yo kid",
+        "author": AUTHORS["firelex"],
+    },
+    {
+        "quote": "There is only 10 kinds of people in the world."
+        + " Those who understand binary, those who don't and those who expected this to be in binary",
+        "author": AUTHORS["osmiumpeach"],
+    },
+    {
+        "quote": "It works on my machine.",
+        "author": AUTHORS["yannick"],
+    },
+    {
+        "quote": "Just type !merch to find some nice things",
+        "author": AUTHORS["hannibal"],
+    },
+    {
+        "quote": "TBH, c'est une meilleure tentative de phishing que les séminaires Excel de Patrice Roy.",
+        "author": AUTHORS["eDavid"],
+    },
+    {
+        "quote": "Click the little star on https://github.com/ArmandBriere/archy (Top right)",
+        "author": AUTHORS["hannibal"],
+    },
+    {
+        "quote": "If JS is the beta, than TS is the buggy release before the Day 1 Patch.",
+        "author": AUTHORS["eDavid"],
+    },
+    {
+        "quote": "Comment est votre blanquette ?",
+        "author": AUTHORS["biereAuGibier"],
+    },
+    {
+        "quote": "J'aime me beurrer la biscotte.",
+        "author": AUTHORS["biereAuGibier"],
+    },
+    {
+        "quote": "Où est Heinrich Von Zimmel ?",
+        "author": AUTHORS["biereAuGibier"],
+    },
+    {
+        "quote": "Une dictature c'est quand les gens sont communistes, déjà. Ils ont froid, "
+        + "des chapeaux gris et des chaussures à fermeture éclair.",
+        "author": AUTHORS["biereAuGibier"],
+    },
+    {
+        "quote": "On va toujours trop loin pour les gens qui ne vont nulle part.",
+        "author": AUTHORS["biereAuGibier"],
+    },
+]
