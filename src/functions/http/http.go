51fa0b28f085e677c8fa07e31ff78408e45ca176
Armand Briere <armand.123@live.fr>
2024-01-15 21:16:42 -0500

    feat: add http in go

---
+++ src/functions/http/http.go
@@ -0,0 +1,53 @@
+package http
+
+import (
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+)
+
+const (
+	httpCatUrl = "https://http.cat/"
+)
+
+type Payload struct {
+	Params []string `json:"params"`
+}
+
+func init() {
+	functions.HTTP("Http", Http)
+}
+
+func Http(w http.ResponseWriter, r *http.Request) {
+
+	var payload = Payload{}
+	err := json.NewDecoder(r.Body).Decode(&payload)
+	if err != nil {
+		slog.Error(fmt.Sprintf("%s: %v", "Cannot parse input payload", err))
+		os.Exit(1)
+	}
+
+	if len(payload.Params) == 0 {
+		fmt.Fprint(w, httpCatUrl+"/418")
+		return
+	}
+
+	requestedUrl := httpCatUrl + payload.Params[0]
+
+	resp, err := http.Get(requestedUrl)
+	if err != nil {
+		slog.Error(fmt.Sprintf("%s: %v", "Cannot get http cat url", err))
+		os.Exit(1)
+	}
+
+	if resp.StatusCode != 200 {
+		fmt.Fprint(w, httpCatUrl+"/404")
+		return
+	}
+
+	fmt.Fprint(w, requestedUrl)
+}
